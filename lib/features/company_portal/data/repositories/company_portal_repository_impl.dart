22e2ddedf919f11f6a182c1ea414268de11d25b6
Abbas <abbas.h.8215@gmail.com>
2025-11-23 21:45:30 +0300

    data  repo

--- lib/features/company_portal/data/repositories/company_portal_repository_impl.dart
+++ lib/features/company_portal/data/repositories/company_portal_repository_impl.dart
@@ -1,15 +1,62 @@
-import 'package:graduation_project/features/company_portal/domain/repositories/company_portal_repository.dart';
 import 'package:graduation_project/features/company_portal/data/data_sources/company_portal_data_source.dart';
+import 'package:graduation_project/features/company_portal/domain/repositories/company_portal_repository.dart';
+import 'package:multiple_result/multiple_result.dart';
+import '../../domain/entities/company_entity.dart';
 
+class CompanyRepositoryImpl implements CompanyRepository {
+  final CompanyRemoteDataSource remote;
+  CompanyRepositoryImpl(this.remote);
 
+  @override
+  Future<Result<CompanyEntity, String>> registerCompany({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final company = await remote.registerCompany(email, password);
+      return Success(company);
+    } catch (e) {
+      return Error(e.toString());
+    }
+  }
 
-/// CompanyPortalRepositoryImpl is the concrete implementation of the CompanyPortalRepository
-/// interface.
-/// This class implements the methods defined in CompanyPortalRepository to interact
-/// with data. It acts as a bridge between the domain layer
-/// (use cases) and the data layer (data sources).
-class CompanyPortalRepositoryImpl implements CompanyPortalRepository {
-      
-   final CompanyPortalDataSource  companyPortalDataSource;
-   CompanyPortalRepositoryImpl(this.companyPortalDataSource);
+  @override
+  Future<Result<CompanyEntity, String>> updateCompanyProfile(
+    CompanyEntity company,
+  ) async {
+    try {
+      final data = await remote.updateProfile(company);
+      return Success(data);
+    } catch (e) {
+      return Error(e.toString());
+    }
+  }
+
+  @override
+  Future<Result<CompanyEntity, String>> getCompanyProfile(String userId) async {
+    try {
+      final data = await remote.getProfile(userId);
+      return Success(data);
+    } catch (e) {
+      return Error(e.toString());
+    }
+  }
+
+  @override
+  Future<Result<List<Map<String, dynamic>>, String>> searchCandidates({
+    String? city,
+    String? skill,
+    String? experience,
+  }) async {
+    try {
+      final results = await remote.searchCandidates(
+        city: city,
+        skill: skill,
+        experience: experience,
+      );
+      return Success(results);
+    } catch (e) {
+      return Error(e.toString());
+    }
+  }
 }
