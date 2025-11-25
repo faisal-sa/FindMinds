1456f2fdc2fd1ee1dc21bec608104cd2fd6311f2
faisal-alfaifi-work <faisal.alfaifi.work@gmail.com>
2025-11-25 10:16:12 +0300

    preparing for merge

--- lib/features/individuals/navigation/pages/individuals_home_page.dart
+++ lib/features/individuals/navigation/pages/individuals_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:graduation_project/core/theme/theme.dart';
 
@@ -76,6 +77,7 @@ class _AppBar extends StatelessWidget implements PreferredSizeWidget {
             backgroundColor: AppColors.blueLight,
             child: Icon(Icons.person, color: AppColors.bluePrimary),
           ),
+          IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app)),
         ],
       ),
     );
